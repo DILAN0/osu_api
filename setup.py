17c0ff5a76a18d4f573d9dae1556145ce3a6e88c
Artem <70542011+DILAN0@users.noreply.github.com>
2024-03-05 11:31:57 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,5 +5,5 @@ setup(
     name='osu_api',
     version='0.1',
     packages=find_packages(),
-    long_description=open(join(dirname(__file__), 'README.txt')).read(),
+    long_description=open(join(dirname(__file__), 'README.rst')).read(),
 )
